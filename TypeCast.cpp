ac73131931f1c26d4b8073f5ed900b36b9951706
Petie Deveer <50847801+ItalianSquirel@users.noreply.github.com>
2023-11-09 18:29:29 -0900

    Update TypeCast.cpp

--- TypeCast.cpp
+++ TypeCast.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-#include <ctime>  // Include this header for seed initialization
+#include <ctime>
 #include <vector>
 #include <string>
 
@@ -20,33 +20,46 @@ int main() {
     // Sample fish names
     std::vector<std::string> fishNames = {"Trout", "Bass", "Salmon"};
 
-    // Print the "title screen"
-    std::cout << "\nWelcome to the game! Press Enter to start...\n";
+    // Game loop
+    while (true) {
+        // Print the "title screen"
+        std::cout << "\nWelcome to the game! Press Enter to start...\n";
 
-    // Wait for the user to press Enter
-    std::cin.get();
+        // Wait for the user to press Enter
+        std::cin.get();
 
-    // Clear the screen
-    clearScreen();
+        // Clear the screen
+        clearScreen();
 
-    // Ask the user if they want to go fishing
-    std::cout << "Want to go fishing? Type the word \"cast\"!\n";
+        // Ask the user if they want to go fishing
+        std::cout << "Want to go fishing? Type the word \"cast\"!\n";
 
-    std::string userInput;
-    std::cin >> userInput;
+        std::string userInput;
+        std::cin >> userInput;
 
-    // Check if the user typed "cast"
-    if (userInput == "cast") {
-        // Generate a random index to select a fish from the vector
-        int randomIndex = std::rand() % fishNames.size();
+        // Check if the user typed "cast"
+        if (userInput == "cast") {
+            // Generate a random index to select a fish from the vector
+            int randomIndex = std::rand() % fishNames.size();
 
-        // Print the selected fish
-        std::cout << "You caught a " << fishNames[randomIndex] << "!\n";
+            // Print the selected fish
+            std::cout << "You caught a " << fishNames[randomIndex] << "!\n";
 
-        // Add the fishing game logic here
-    } else {
-        std::cout << "Invalid input. Game over.\n";
+            // Ask if the player wants to play again
+            std::cout << "Do you want to play again? (yes/no): ";
+            std::cin >> userInput;
+
+            // Check if the user wants to play again
+            if (userInput != "yes") {
+                break; // Exit the game loop
+            }
+        } else {
+            std::cout << "Invalid input. Game over.\n";
+            break; // Exit the game loop
+        }
     }
+
+    std::cout << "Thanks for playing!\n";
 
     return 0;
 }
