0447b26edd00b369d5555d299f6eba083eb81ac7
ItalianSquirel <petiedeveer@gmail.com>
2023-11-09 19:41:22 -0900

    Changes to be committed: 	modified:   TypeCast.cpp  Untracked files: 	a.out

--- TypeCast.cpp
+++ TypeCast.cpp
@@ -4,17 +4,37 @@
 #include <vector>
 #include <string>
 
-//represents a species of fish with arbitrary (but functional) default values
-struct Fish {
+// Represents a species of fish with arbitrary (but functional) default values
+struct FishSpecies {
     std::string name = "unnamed fish";
     float minWeight = 0.5f;
     float maxWeight = 25;
     float minLength = 4;
     float maxLength = 64;
 
-    //constructor
-    Fish(std::string n, int minW, int maxW, int minL, int maxL) : name(n), minWeight(minW), maxWeight(maxW), minLength(minL), maxLength(maxL) {
-        //init code
+    // Constructor
+    FishSpecies(std::string n, int minW, int maxW, int minL, int maxL)
+        : name(n), minWeight(minW), maxWeight(maxW), minLength(minL), maxLength(maxL) {
+        // Init code
+    }
+};
+
+// Represents an instance of a caught fish with calculated stats
+struct CaughtFish {
+    std::string name = "unnamed fish";
+    float weight = 0.0f;
+    float length = 0.0f;
+
+    // Constructor using a FishSpecies
+    CaughtFish(const FishSpecies& species)
+        : name(species.name), weight(generateRandom(species.minWeight, species.maxWeight)),
+          length(generateRandom(species.minLength, species.maxLength)) {
+        // Init code
+    }
+
+    // Function to generate a random float in a given range
+    static float generateRandom(float min, float max) {
+        return min + static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX / (max - min)));
     }
 };
 
@@ -31,9 +51,9 @@ int main() {
     // Seed the random number generator
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-    // Sample fish names
-    std::vector<Fish*> fishes; //this would be a memory leak but it's ok because there is a fixed number of fish
-    fishes.push_back(new Fish("Salmon", 1, 25, 8, 36));
+    // Sample fish species
+    std::vector<FishSpecies> fishSpecies;
+    fishSpecies.push_back(FishSpecies("Salmon", 1, 25, 8, 36));
 
     // Game loop
     while (true) {
@@ -54,17 +74,23 @@ int main() {
 
         // Check if the user typed "cast"
         if (userInput == "cast") {
-            // Generate a random index to select a fish from the vector
-            int randomIndex = std::rand() % fishes.size();
+            // Generate a random index to select a fish species from the vector
+            int randomIndex = std::rand() % fishSpecies.size();
 
-            // Print the selected fish
-            std::cout << "You've got a " << fishes[randomIndex]->name << " on the hook!\nType its name to reel it in!\n";
+            // Create an instance of CaughtFish based on the selected fish species
+            CaughtFish caughtFish(fishSpecies[randomIndex]);
+
+            // Print the details of the caught fish
+            std::cout << "You've got a " << caughtFish.name << " on the hook!\n";
+            std::cout << "Type its name to reel it in!\n";
             std::cin >> userInput;
-            if (userInput == fishes[randomIndex]->name) {
-                std::cout << "you caught a " << fishes[randomIndex]->name << "\n";
-            }
-            else {
-                std::cout << "oops! It broke away!";
+
+            // Check if the user entered the correct fish name
+            if (userInput == caughtFish.name) {
+                std::cout << "You caught a " << caughtFish.name << "!\n";
+                std::cout << "Weight: " << caughtFish.weight << " lbs, Length: " << caughtFish.length << " inches\n";
+            } else {
+                std::cout << "Oops! It broke away!\n";
             }
 
             // Ask if the player wants to play again
@@ -85,3 +111,4 @@ int main() {
 
     return 0;
 }
+
